af560f616358c947938659f35cdac591688655c5
Illanicz Barnabás <illanicz.barnabas@autsoft.hu>
2019-02-15 17:37:33 +0100

    - Fixed IndexPath init parameters

--- Sources/JTAppleCalendarLayout.swift
+++ Sources/JTAppleCalendarLayout.swift
@@ -386,7 +386,7 @@ class JTAppleCalendarLayout: UICollectionViewLayout, JTAppleCalendarLayoutProtoc
                         if missCount > maxMissCount && beganIntercepting { break outterLoop }
                     }
                     if thereAreDecorationViews {
-                        let attrib = layoutAttributesForDecorationView(ofKind: decorationViewID, at: IndexPath(item: 0, section: sectionIndex))!
+                        let attrib = layoutAttributesForDecorationView(ofKind: decorationViewID, at: IndexPath(item: val.0, section: val.1))!
                         attributes.append(attrib)
                     }
                 }
